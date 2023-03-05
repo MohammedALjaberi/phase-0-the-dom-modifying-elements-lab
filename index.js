d2034f1b12f9e3bfa9da403d08a0b68bc4499c3d
Mohammed Al-jaberi <aljabri606@gmail.com>
2023-03-05 14:20:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const mani = document.querySelector('main#main');
+mani.remove();
+const body = document.getElementsByTagName("body")
+const newHeader = document.createElement("h1 id=victory");
+newHeader.textContent = "Mohammed is the champion" ; 
